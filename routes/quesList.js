5e4f4eecc7f3d51f4eb161cf01a9b022257b49a1
jake <xiaomi168168@gmail.com>
2017-09-11 10:41:43 +0800

    9.11 zfw
    9.11 zfw

--- routes/quesList.js
+++ routes/quesList.js
@@ -13,12 +13,17 @@ router.get('/', function(req, res, next) {
   if(typeof (req.session.name) === 'undefined'){
     req.session.name = null;
   }
-  console.log(".......",db.question_list.find({}));
   db.question_list.find().sort({'_id': -1}).exec(function(err, doc) {
     if(err) console.log(err);
     console.log(doc);
     res.render('quesList', { title: 'TodoList' ,name: req.session.name, data: doc, moment: moment});
   });
+
+  if(req.query.user_label) {
+    db.question_list.find({user_label: req.query.user_label}).sort({'_id': -1}).exec(function(err, doc) {
+      console.log()
+    })
+  }
 
 });
 
